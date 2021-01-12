f89b754a118e7c7cbaaf10d429147761bf915f2a
Zhengtong Liu <zhengtongliu@outlook.com>
2021-01-12 09:36:57 +0800

    move options to lab0.c

--- lab0/lab0.c
+++ lab0/lab0.c
@@ -10,11 +10,18 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <string.h>
-
-#include "./options.h"
+#include <getopt.h>
 
 #define BUFFERSIZE 1024
 char buffer[BUFFERSIZE];
+
+struct opts {
+    bool valid;
+    bool segfault;
+    bool catch_flag;
+    char* input;
+    char* output;
+};
 
 void sigsegv_handler (int sig) {
     if (sig == SIGSEGV) 
@@ -22,6 +29,59 @@ void sigsegv_handler (int sig) {
         fprintf(stderr, "a segmentation fault has bee detected.\n");
         exit(4);
     }
+}
+
+void read_options(
+    int argc,
+    char* argv[],
+    struct opts* opts
+) {
+    opts -> valid = false;
+    opts -> segfault = false;
+    opts -> catch_flag = false;
+    opts -> input = NULL;
+    opts -> output = NULL;
+
+    int opt = 0;
+    static struct option long_options[] = {
+        {"input", required_argument, 0, 'i'},
+        {"output", required_argument, 0, 'o'},
+        {"segfault", no_argument, 0, 's'},
+        {"catch", no_argument, 0, 'c'},
+        {0, 0, 0, 0}
+    };
+
+    int long_index = 0;
+    bool flag = true;
+    while ((opt = getopt_long(argc, argv, "", 
+                    long_options, &long_index)) != -1) {
+            switch (opt)
+            {
+            case 'i':
+                opts -> input = optarg;
+                break;
+            case 'o':
+                opts -> output = optarg;
+                break;
+            case 's':
+                opts -> segfault = true;
+                break;
+            case 'c':
+                opts -> catch_flag = true;
+                break;
+            default:
+                flag = false;
+            }
+        }
+    if (optind < argc)
+    {
+        flag = false;
+        printf ("non-option ARGV-elements: ");
+        while (optind < argc)
+            printf("%s", argv[optind++]);
+        putchar ('\n');
+    }
+    opts -> valid = flag;
 }
 
 int
